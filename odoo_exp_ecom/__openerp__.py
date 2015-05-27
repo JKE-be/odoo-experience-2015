c7a21f0d4ad48cb8ca9ca1d1cfcd25ce881eb1ba
Jeremy Kersten <jke@odoo.com>
2015-05-27 22:43:37 +0200

    INIT

---
+++ odoo_exp_ecom/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_exp_ecom",
+    'summary': """
+        Odoo Experience 2015 - Ecommerce
+    """,
+    'description': """
+        Odoo experience - How to customize E-Commerce
+    """,
+    'author': "jke-odoo",
+    'website': "https://www.odoo.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['website_sale'],
+    'data': [
+        'templates.xml',
+    ],
+
+}
